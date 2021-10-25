a612cbee8a91633f8a556a3b55cbae6b641defc6
Yuval Koren <mygz.koren@gmail.com>
2021-10-25 14:24:52 -0700

    Wrap OutputStream in a Swift 5.5 Actor

---
+++ Sources/AsyncStream/OutputStreamActor.swift
@@ -0,0 +1,109 @@
+//  OutputStreamActor.swift
+//
+//  Created by Yuval Koren on 10/25/21.
+//  Copyright © 2021 Appcessori Corporation.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+public enum StreamActorError: Error {
+    case NotOpen
+    case WriteError
+}
+
+public actor OutputStreamActor: NSObject {
+    let output: OutputStream
+    private var yield: ((Bool)->())?
+    private var finish:(()->())?
+    
+    init(_ output: OutputStream) {
+        self.output = output
+        super.init()
+        output.delegate = self
+        output.schedule(in: RunLoop.current, forMode: RunLoop.Mode.default)
+        output.open()
+    }
+    
+    func getSpaceAvailableStream()-> AsyncStream<Bool> {
+        AsyncStream<Bool> { continuation in
+            yield = { spaceAvailable in
+                continuation.yield(spaceAvailable)
+            }
+            finish = {
+                continuation.finish()
+            }
+        }
+    }
+    
+    func write(_ data: Data) async throws -> Int {
+        guard output.streamStatus == Stream.Status.open else {
+            throw StreamActorError.NotOpen
+        }
+        var totalBytesWritten = 0
+        try data.withUnsafeBytes { unsafeRawBufferPointer in
+            while output.hasSpaceAvailable == true && totalBytesWritten < data.count {
+                try Task.checkCancellation()
+                guard let unsafeBasePointer = unsafeRawBufferPointer.bindMemory(to: UInt8.self).baseAddress else {
+                    fatalError()
+                }
+                let unsafePointer = unsafeBasePointer + totalBytesWritten
+                let bytesWritten = output.write(unsafePointer, maxLength: data.count - totalBytesWritten)
+                if bytesWritten == -1 {
+                    throw StreamActorError.WriteError
+                }
+                if bytesWritten > 0 {
+                    totalBytesWritten += bytesWritten
+                }
+            }
+            if !output.hasSpaceAvailable {
+                hasSpaceAvailable(false)
+            }
+        }
+        return totalBytesWritten
+    }
+    
+    private func hasSpaceAvailable(_ value: Bool) {
+        yield?(value)
+    }
+    
+    deinit {
+        print(#function)
+        output.close()
+        output.remove(from: RunLoop.current, forMode: RunLoop.Mode.default)
+        output.delegate = nil
+    }
+}
+
+extension OutputStreamActor: StreamDelegate {
+    nonisolated public func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+        guard aStream is OutputStream else {
+            fatalError("\(#function) Expected OutputStream")
+        }
+        print(#function, "OutputStream" , eventCode)
+        switch eventCode {
+        case Stream.Event.hasSpaceAvailable:
+            Task.detached { [weak self] in
+                await self?.hasSpaceAvailable(true)
+            }
+        default:
+            break
+        }
+    }
+}
--- Tests/AsyncStreamTests/AsyncStreamTests.swift
+++ Tests/AsyncStreamTests/AsyncStreamTests.swift
@@ -2,10 +2,64 @@ import XCTest
 @testable import AsyncStream
 
 final class AsyncStreamTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AsyncStream().text, "Hello, World!")
+    static var streamBufferSize = 4096
+    var inputStream: InputStream!
+    var outputStream: OutputStream!
+
+    override func setUp() {
+        var optionalInputStream: InputStream?
+        var optionalOutputStream: OutputStream?
+        Stream.getBoundStreams(withBufferSize: Self.streamBufferSize, inputStream: &optionalInputStream, outputStream: &optionalOutputStream)
+        self.inputStream = optionalInputStream!
+        self.outputStream = optionalOutputStream!
+    }
+    
+    func testSingleWrite() async throws {
+        let expectation = self.expectation(description: "write succeeded")
+        let outputActor = OutputStreamActor(outputStream)
+        let writeReadyStream = await outputActor.getSpaceAvailableStream()
+        Task {
+            for await writeReady in writeReadyStream {
+                if writeReady {
+                    let size = Self.streamBufferSize >> 2
+                    let data = Data.init(count: size)
+                    let bytesWritten = try await outputActor.write(data)
+                    XCTAssert(bytesWritten == size)
+                    expectation.fulfill()
+                    break
+                }
+            }
+        }
+        RunLoop.current.run(until: Date())
+        await waitForExpectations(timeout: 2.0) { error in
+            if let error = error {
+                XCTFail(error.localizedDescription)
+            }
+        }
+    }
+    
+    func testWriteUntilFull() async throws {
+        let expectation = self.expectation(description: "stream buffer full")
+        let outputActor = OutputStreamActor(outputStream)
+        let writeReadyStream = await outputActor.getSpaceAvailableStream()
+        Task {
+            for await writeReady in writeReadyStream {
+                if writeReady {
+                    let size = Self.streamBufferSize >> 2
+                    let data = Data.init(count: size)
+                    let bytesWritten = try await outputActor.write(data)
+                    XCTAssert(bytesWritten == size)
+                    RunLoop.current.run(until: Date())
+                } else {
+                    expectation.fulfill()
+                }
+            }
+        }
+        RunLoop.current.run(until: Date())
+        await waitForExpectations(timeout: 2.0) { error in
+            if let error = error {
+                XCTFail(error.localizedDescription)
+            }
+        }
     }
 }
